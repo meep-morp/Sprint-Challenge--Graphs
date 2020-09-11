d2a03fe71c2927dbe66092be7c13645b8808bdd3
meep-morp <gracieaerogirl@gmail.com>
2020-09-11 11:56:18 -0500

    Finished MVP

--- adv.py
+++ adv.py
@@ -17,7 +17,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -26,9 +26,82 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
-traversal_path = []
 
+# traversal_path = ['n', 'n']
+opposites = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
+
+#  Find all available paths
+
+
+def find_paths(first_room, visited=set()):
+    cur_visited = set()
+
+    for room in visited:
+        cur_visited.add(room)
+    path = []
+
+    def add_to_path(room, back=None):
+        cur_visited.add(room)
+        exits = room.get_exits()
+
+        for direction in exits:
+            if room.get_room_in_direction(direction) not in cur_visited:
+                path.append(direction)
+                add_to_path(room.get_room_in_direction(
+                    direction), opposites[direction])
+        # If we can go back
+        if back:
+            path.append(back)
+
+    add_to_path(first_room)
+    return path
+
+
+def shortest_path(first_room, visited=set()):
+    path = []
+
+    # Function to find the path
+    def add_to_path(room, back=None):
+        visited.add(room)
+        exits = room.get_exits()
+        path_lengths = {}
+
+        # Recurse through find_paths for every direction
+        # BFS approach
+        for direction in exits:
+            path_lengths[direction] = len(find_paths(
+                room.get_room_in_direction(direction), visited))
+
+        # Init the order we are going to take
+        traverse_order = []
+
+        # Sort by shortest path first
+        sorted_paths = sorted(path_lengths.items(), key=lambda x: x[1])
+
+        # Add the sorted directions to order
+        for key, _ in sorted_paths:
+            traverse_order.append(key)
+
+        for direction in traverse_order:
+            # If not visited ->
+            if room.get_room_in_direction(direction) not in visited:
+                path.append(direction)
+                add_to_path(room.get_room_in_direction(
+                    direction), opposites[direction])
+
+        # If the length of visited is the same as rooms, we're done
+        if len(visited) == len(world.rooms):
+            return
+        # Else keep going
+        elif back:
+            path.append(back)
+
+    # Run through first room
+    add_to_path(first_room)
+    return path
+
+
+traversal_path = shortest_path(world.starting_room)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -41,22 +114,22 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
-
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
